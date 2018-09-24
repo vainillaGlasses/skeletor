d8343ec87f705190825e771f4ab9b3648777dd1e
Samantha Arburola <samantha@estudiomanati.com>
2018-09-24 16:01:05 -0600

    block

---
+++ modules/custom/faq_module/src/Plugin/Block/faq_accordion.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\faq_module\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'faq_accordion' block.
+ *
+ * @Block(
+ *  id = "faq_accordion",
+ *  admin_label = @Translation("Faq_accordion"),
+ * )
+ */
+class faq_accordion extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['faq_accordion']['#markup'] = 'Implement faq_accordion.';
+
+    return $build;
+  }
+
+}
